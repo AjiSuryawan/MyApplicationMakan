40b40ed942bf2f989d1fa3126bca5762c6a222a7
ajisuryawan123 <kazumovie132@gmail.com>
2018-11-24 18:46:31 +0700

    update

--- app/src/main/java/com/example/user1/myapplication/Home.java
+++ app/src/main/java/com/example/user1/myapplication/Home.java
@@ -1,24 +1,40 @@
 package com.example.user1.myapplication;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.user1.myapplication.Database.MahasiswaHelper;
 import com.example.user1.myapplication.Model.ModelDatabase;
 
+import pub.devrel.easypermissions.EasyPermissions;
+
 public class Home extends AppCompatActivity {
 
+    final int PICK_FROM_GALLERY=1001;
     EditText txtdata1;
     EditText txtdata2;
+    ImageView ivku;
     Button btnsave;
+    Button btnfoto;
     MahasiswaHelper mahasiswaHelper;
     ModelDatabase data;
 
@@ -44,9 +60,28 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         mahasiswaHelper = new MahasiswaHelper(Home.this);
-
+        ivku=(ImageView)findViewById(R.id.ivgambar);
         txtdata1 = (EditText) findViewById(R.id.txtdata1);
         txtdata2 = (EditText) findViewById(R.id.txtdata2);
+        btnfoto = (Button) findViewById(R.id.btnfoto);
+        btnfoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String[] galleryPermissions = {Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+                if (EasyPermissions.hasPermissions(Home.this, galleryPermissions)) {
+                    Intent i = new Intent(Intent.ACTION_PICK,
+                            android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                    startActivityForResult(i, PICK_FROM_GALLERY);
+                } else {
+                    EasyPermissions.requestPermissions(Home.this,"Access for storage",PICK_FROM_GALLERY,galleryPermissions);
+                }
+
+
+
+
+            }
+        });
         btnsave = (Button) findViewById(R.id.btnsave);
         btnsave.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,4 +104,29 @@ public class Home extends AppCompatActivity {
             }
         });
     }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch(requestCode) {
+            case PICK_FROM_GALLERY:
+                if(resultCode == RESULT_OK){
+                    Uri selectedImage = data.getData();
+                    String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+                    Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+                    cursor.moveToFirst();
+
+                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                    String filePath = cursor.getString(columnIndex);
+                    cursor.close();
+
+
+                    Bitmap yourSelectedImage = BitmapFactory.decodeFile(filePath);
+                    ivku.setImageBitmap(yourSelectedImage);
+                    /* Now you have choosen image in Bitmap format in object "yourSelectedImage". You can use it in way you want! */
+                }
+        }
+
+    };
 }
